a91efa99eacce9c37ea80953855aa7f4308781cc
juze117 <juzengler@gmail.com>
2019-09-26 18:44:15 +0200

    main

--- Innlevering 5/temperatur.py
+++ Innlevering 5/temperatur.py
@@ -1,20 +1,8 @@
 '''
 skriver ut gjennomsnittstemperatur fra fil med månedlige temperaturer
 '''
-
 # importer urllib
 import urllib.request
-
-# Url til fil med temperaturer
-oblig_url = "https://www.uio.no/studier/emner/matnat/ifi/IN1000/h19/Obligatoriske-innleveringer/temperatur.txt"
-# åpne url-filen med urllib
-respons = urllib.request.urlopen(oblig_url)
-
-# tom liste for å holde temperaturene
-tempAar = []
-# legg til hver temperatur som flyttal i listen
-for linje in respons:
-    tempAar.append( float(linje) )
 
 # 2
 # funksjon gjennomsnitt
@@ -32,5 +20,20 @@ def gjennomsnitt(a):
     # returner summen av temperaturene delt på antallet oppføringer
     return total / len(a)
 
-# Skriv gjennomsnittstemperaturen ved å kalle på funksjonen gjenomsnitt
-print( "Gjennomsnittstemperatur:", gjennomsnitt(tempAar) )
+
+def hovedprogram():
+    # Url til fil med temperaturer
+    oblig_url = "https://www.uio.no/studier/emner/matnat/ifi/IN1000/h19/Obligatoriske-innleveringer/temperatur.txt"
+    # åpne url-filen med urllib
+    respons = urllib.request.urlopen(oblig_url)
+
+    # tom liste for å holde temperaturene
+    tempAar = []
+    # legg til hver temperatur som flyttal i listen
+    for linje in respons:
+        tempAar.append( float(linje) )
+
+    # Skriv gjennomsnittstemperaturen ved å kalle på funksjonen gjenomsnitt
+    print( "Gjennomsnittstemperatur:", gjennomsnitt(tempAar) )
+
+hovedprogram()
