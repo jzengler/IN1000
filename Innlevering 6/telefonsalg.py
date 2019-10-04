453d1b69e3ab8639de6b9a78809ffe14a14f5018
juze117 <juzengler@gmail.com>
2019-10-04 14:12:09 +0200

    open file oppg 1

---
+++ Innlevering 6/telefonsalg.py
@@ -0,0 +1,17 @@
+# importer urllib
+import urllib.request
+
+
+def innlesing(filnavn):
+
+    # Opprett tom ordbok til salgstall for selgere
+    salg = {}
+
+    # Les fil fra lokal filbane
+    with open(filnavn, "r") as salgsfil:
+
+        # split hver linje på mellomrom og fjern newline
+        # bruk navn som nøkkel og salgstall som verdi i ordbok
+        for selger in salgsfil:
+            s = selger.rstrip("\n").split(" ")
+            salg[s[0]] = int(s[1])
